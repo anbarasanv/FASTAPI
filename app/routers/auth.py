bc9ac41d38ea697a307375453da5332f5604b79b
anbarasanv <33603902+anbarasanv@users.noreply.github.com>
2021-12-10 16:24:43 +0530

    updated-response-model-and-status-code

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -5,20 +5,19 @@ from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from app import models
-from .. import database, schema, utility, oauth2
+from .. import database, utility, oauth2, schema
 
 
 router = APIRouter(tags=["Authentication"])
 
-@router.post('/login')
+@router.post('/login', response_model=schema.Token, status_code=status.HTTP_200_OK)
 def login(user_cred: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
     user = db.query(models.User).filter(models.User.email == user_cred.username).first()
-
     if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                            detail='Invalid credentials name')
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Incorrect username or password")
     if not utility.verify(user_cred.password, user.password):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid credentials pwd')
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Incorrect username or password")
 
     access_token = oauth2.get_accesss_token(data={"user_id": user.id})
-    return {"access_token": access_token, "token_type":"bearer"}
+
+    return {"access_token": str(access_token), "token_type": "bearer"}
