459dd616b6280a473cfd40a1efbaf9c947de2af2
anbarasanv <33603902+anbarasanv@users.noreply.github.com>
2021-12-11 06:57:11 +0530

    renamed-current-user

--- app/routers/post.py
+++ app/routers/post.py
@@ -12,24 +12,29 @@ from ..oauth2 import get_current_user
 router = APIRouter(prefix="/posts", tags=["Posts"])
 
 
-@router.get('/', response_model=List[PostResponse])
+@router.get("/", response_model=List[PostResponse])
 async def get_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
     return posts
 
 
-@router.get('/{id}', status_code=status.HTTP_200_OK, response_model=PostResponse)
+@router.get("/{id}", status_code=status.HTTP_200_OK, response_model=PostResponse)
 async def get_posts(id: int, db: Session = Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
-        raise HTTPException(status_code=status.HTTP_204_NO_CONTENT,
-                            detail=f"The post id: {id} did not found")
+        raise HTTPException(
+            status_code=status.HTTP_204_NO_CONTENT,
+            detail=f"The post id: {id} did not found",
+        )
     return post
 
 
-@router.post('/', status_code=status.HTTP_201_CREATED, response_model=PostResponse)
-async def create_posts(post: Post, db: Session = Depends(get_db),
-                       user_id: int = Depends(get_current_user)):
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=PostResponse)
+async def create_posts(
+    post: Post,
+    db: Session = Depends(get_db),
+    current_user: int = Depends(get_current_user),
+):
     new_post = models.Post(**post.dict())
     db.add(new_post)
     db.commit()
@@ -38,28 +43,38 @@ async def create_posts(post: Post, db: Session = Depends(get_db),
     return new_post
 
 
-@router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
-async def delete_post(id: int, db: Session = Depends(get_db),
-                      user_id: int = Depends(get_current_user)):
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_post(
+    id: int,
+    db: Session = Depends(get_db),
+    current_user: int = Depends(get_current_user),
+):
     post = db.query(models.Post).filter(models.Post.id == id)
     ret_data = post.first()
     if not post.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"The requested id: {id} content not found")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"The requested id: {id} content not found",
+        )
     post.delete(synchronize_session=False)
     db.commit()
     # db.refresh(ret_data)
     return ret_data
 
 
-@router.put('/{id}', status_code=status.HTTP_200_OK, response_model=PostResponse)
-async def update_post(id: int, post: Post, db: Session = Depends(get_db),
-                      user_id: int = Depends(get_current_user)):
+@router.put("/{id}", status_code=status.HTTP_200_OK, response_model=PostResponse)
+async def update_post(
+    id: int,
+    post: Post,
+    db: Session = Depends(get_db),
+    current_user: int = Depends(get_current_user),
+):
     post_data = db.query(models.Post).filter(models.Post.id == id)
     ret_data = post_data.first()
     if not post_data.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"updating id: {id} not found")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"updating id: {id} not found"
+        )
     post_data.update(post.dict(), synchronize_session=False)
     db.commit()
     db.refresh(ret_data)
