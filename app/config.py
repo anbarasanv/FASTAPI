4e8d68f72437eabf49c8828575cd201f95c285c5
anbarasanv <33603902+anbarasanv@users.noreply.github.com>
2021-12-14 06:19:37 +0530

    implementation-for-using-env-variables

---
+++ app/config.py
@@ -0,0 +1,19 @@
+from pydantic import BaseSettings
+
+# from dotenv import load_dotenv
+
+# loading environment variables from the .env file
+# load_dotenv()
+
+
+class Settings(BaseSettings):
+    SQL_ALCHEMY_DATABASE_URL: str
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
