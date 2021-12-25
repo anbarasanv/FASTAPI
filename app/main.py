3c38415999ee3c3de514b26d8d5f8677ad0d632f
anbarasanv <33603902+anbarasanv@users.noreply.github.com>
2021-12-25 07:24:07 +0530

    updated-midleware

--- app/main.py
+++ app/main.py
@@ -1,15 +1,32 @@
 """This the main package for the application
 """
 from fastapi import FastAPI
-from fastapi.params import Depends
-from . import models
-from .database import engine
+
+# from fastapi.params import Depends
+# from . import models
+# from .database import engine
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import user, post, auth, vote
 
 # This line creates the required database tables,if not exists already
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
 
 app.include_router(user.router)
 app.include_router(post.router)
